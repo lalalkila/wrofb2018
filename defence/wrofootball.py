37ab05b26b72ae32060860cc1914368fd74aeae1
lalalkila <40801045+lalalkila@users.noreply.github.com>
2018-07-18 19:58:56 +0700

    base file

--- defence/wrofootball.py
+++ defence/wrofootball.py
@@ -10,11 +10,11 @@ def Init():
     assert color_sensor_2.connected(), "color_sensor_2 undetected!"
     color_sensor_2.mode = 'COL-AMBIENT'
 
-    IR_seeker = HiTechnc.NewIRDIR()
+    IR_seeker = Sensor(address = 'in4', driver = 'ht-nxt-ir-seek-v2')
     assert IR_seeker.connected(), "IR_seeker-v2 undetected!"
     IR_seeker.mode = 'DC'
 
-    compass_sensor = HiTechnc.Compass()
+    compass_sensor = Sensor(address = 'in2', driver = 'ht-nxt-compass')
     assert compass_sensor.connected(), "compass_sensor undetected"
 
     motorA = MediumMotor('outA')
@@ -22,13 +22,28 @@ def Init():
     motorC = MediumMotor('outC')
     motorD = MediumMotor('outD')
 
+    return color_sensor_1, color_sensor_2, IR_seeker, compass_sensor, motorA, motorB, motorC, motorD
+
 class compass():
+    global compass_sensor, main_heading
     def __init__():
-        compass.relative = ((origin_heading - compass_sensor.value() + 180) % 360) - 180
+        compass.relative = ((main_heading - compass_sensor.value() + 180) % 360) - 180
         compass.direction = compass_sensor.value()
 
 
+def CanBang():
+    if not compass.relative() < 3 && compass.relative() > -3:
+        while True:
+            if compass.relative > 3:
+                move(-1000, -1000, 0.01)
+            elif compass.relative < -3:
+                move(1000, 1000, 0.01)
+            else:
+                break
+
+
 def move(Vx, Vy, omega = 0, radius = 1):
+    global motorA, motorB, motorC, motorD
     motorA.run_forever(omega * radius - Vx * cos(45) - Vy * sin(45))
     motorB.run_forever(omega * radius - Vx * sin(45) + Vy * cos(45))
     motorC.run_forever(omega * radius + Vx * sin(45) + Vy * cos(45))
@@ -36,6 +51,7 @@ def move(Vx, Vy, omega = 0, radius = 1):
 
 
 def ProcessInput():
+    global IR_seeker
     raw_value = IR_seeker.value()
     if raw_value == 5:
         return 5
@@ -46,12 +62,33 @@ def ProcessInput():
     else:
         return 6
 
-def update(main_heading, ir_direction):a
+def update(main_heading, ir_direction):
     if ir_direction == 5:
         pass
     elif ir_direction == 4:
         while compass.relative(main_heading) >= 45:
             move(1000, 1000, 0.01)
-        
-        
-        
+        while Ev3.MotorRotation() < 0.5:
+            move(1000, 1000, 0)
+        while IR_seeker.value() > 5:
+            move(-1000,-1000,0)
+        CanBang()
+    elif ir_direction == 6:
+        while compass.relative(main_heading) >= -45:
+            move(-1000, -1000, 0.01)
+        while Ev3.MotorRotation() < 0.5:
+            move(-1000, -1000, 0)
+        while IR_seeker.value() > 5:
+            move(1000,1000,0)
+        CanBang()
+    else:
+        CanBang()
+
+while __name__=='__main__':
+    color_sensor_1, color_sensor_2, IR_seeker, compass_sensor, motorA, motorB, motorC, motorD = Init()
+    main_heading = compass.direction()
+    while True:
+        ir_direction = ProcessInput()
+        update(main_heading, ir_direction)
+        except Exception:
+            #Quitting
